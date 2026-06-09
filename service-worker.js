04d0c958a83dff944a6188ba23db40bb0f5bd462
fruity-muscat <uokuma_osakana@yahoo.co.jp>
2026-06-09 21:47:00 +0900

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,97 @@
+const CACHE_NAME = "ver6pro-v1";
+
+const urlsToCache = [
+    "./",
+    "./Ver6Pro.html",
+    "./manifest.json",
+    "./icon-192.png",
+    "./icon-512.png"
+];
+
+
+/* インストール */
+
+self.addEventListener(
+    "install",
+    event => {
+
+        event.waitUntil(
+
+            caches.open(CACHE_NAME)
+
+            .then(cache => {
+
+                return cache.addAll(
+                    urlsToCache
+                );
+
+            })
+
+        );
+
+    }
+);
+
+
+/* リクエスト */
+
+self.addEventListener(
+    "fetch",
+    event => {
+
+        event.respondWith(
+
+            caches.match(
+                event.request
+            )
+
+            .then(response => {
+
+                return response ||
+
+                fetch(event.request);
+
+            })
+
+        );
+
+    }
+);
+
+
+/* 更新 */
+
+self.addEventListener(
+    "activate",
+    event => {
+
+        event.waitUntil(
+
+            caches.keys()
+
+            .then(keys => {
+
+                return Promise.all(
+
+                    keys.map(key => {
+
+                        if(
+                            key !== CACHE_NAME
+                        ){
+
+                            return caches.delete(
+                                key
+                            );
+
+                        }
+
+                    })
+
+                );
+
+            })
+
+        );
+
+    }
+);
